1e74e70b67bc3e70a7d82e44a5c6242ab3822122
benkenobi007 <ktiwari998@gmail.com>
2019-10-28 02:02:37 +0530

    Functionalize shader

--- opengl_practice_1/fragment_core.glsl
+++ opengl_practice_1/fragment_core.glsl
@@ -15,35 +15,53 @@ in vec3 vs_normal;
 
 out vec4 fs_color;
 
+//Uniforms
 uniform Material material;
-
 uniform vec3 lightPos0;
 uniform vec3 camPosition;
+
+//Functions
+
+vec3 calculateAmbient(Material material){
+	return material.ambient;
+}
+
+vec3 calculateDiffuse(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0)
+{
+	vec3 posToLightDirVec = normalize(lightPos0 - vs_position);
+	float diffuse = clamp(dot(vs_normal, posToLightDirVec), 0, 1);
+	vec3 diffuseFinal = material.diffuse * diffuse;
+
+	return diffuseFinal;
+}
+
+vec3 calculateSpecular(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0, vec3 camPosition){
+	vec3 lightToPosDirVec = normalize(vs_position - lightPos0);
+	vec3 reflectedDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
+	vec3 posToViewDirVec = normalize(camPosition - vs_position);
+	float specularConstant = pow(max(dot(reflectedDirVec, posToViewDirVec), 0), 10);
+	vec3 specularFinal = material.specular * specularConstant;
+
+	return specularFinal;
+}
 
 void main(){
 //	fs_color = vec4(vs_color, 1.f);
 //	fs_color = texture(texture0, vs_texcoord) + texture(texture1, vs_texcoord);
 
 	//Ambient Lighting
-	vec3 ambientLight = material.ambient;
+	vec3 ambientFinal = calculateAmbient(material);
 
 	//Diffuse Lighting
-	vec3 posToLightDirVec = normalize(lightPos0 - vs_position);
-	//vec3 diffuseColor = vec3(1.f, 1.f, 1.f);
-	float diffuse = clamp(dot(vs_normal, posToLightDirVec), 0, 1);
-	vec3 diffuseFinal = material.diffuse * diffuse;
+	vec3 diffuseFinal = calculateDiffuse(material, vs_position, vs_normal, lightPos0);
 
 	//Specular Lighting
-	vec3 lightToPosDirVec = normalize(vs_position - lightPos0);
-	vec3 reflectedDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
-	vec3 posToViewDirVec = normalize(camPosition - vs_position);
-	float specularConstant = pow(max(dot(reflectedDirVec, posToLightDirVec), 0), 10);
-	vec3 specularFinal = material.specular * specularConstant;
+	vec3 specularFinal = calculateSpecular(material, vs_position, vs_normal, lightPos0, camPosition);
 
 	//Attenuation
 
 	//Final light
 	fs_color = texture(material.diffuseTex, vs_texcoord) 
 	* vec4(vs_color, 1.f)
-	* (vec4(ambientLight, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
+	* (vec4(ambientFinal, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
 }

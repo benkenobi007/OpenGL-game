d1c8eaca0ba6ec9b888115c442250e7dce1e13f6
benkenobi007 <ktiwari998@gmail.com>
2019-11-01 16:52:02 +0530

    Ground textures

--- opengl_practice_1/Game.cpp
+++ opengl_practice_1/Game.cpp
@@ -102,6 +102,10 @@ void Game::initTextures()
 	this->textures.push_back(new Texture("Images/pic2.png", GL_TEXTURE_2D));
 	this->textures.push_back(new Texture("Images/pic2_specular.png", GL_TEXTURE_2D));
 
+	//GROUND
+	this->textures.push_back(new Texture("Images/pic3.png", GL_TEXTURE_2D));
+	this->textures.push_back(new Texture("Images/pic3_specular.png", GL_TEXTURE_2D));
+
 }
 
 void Game::initMaterials()
@@ -128,7 +132,7 @@ void Game::initModels()
 	);
 
 	this->models.push_back(new Model(
-		glm::vec3(1.f, 0.f, 0.f),
+		glm::vec3(2.f, 0.f, 5.f),
 		this->materials[0],
 		this->textures[TEX_PIC1],
 		this->textures[TEX_PIC1_SPECULAR],
@@ -137,7 +141,7 @@ void Game::initModels()
 	);
 
 	this->models.push_back(new Model(
-		glm::vec3(0.f, 1.f, 1.f),
+		glm::vec3(2.f, 1.f, -3.f),
 		this->materials[0],
 		this->textures[TEX_PIC2],
 		this->textures[TEX_PIC2_SPECULAR],
@@ -149,15 +153,81 @@ void Game::initModels()
 		glm::vec3(2.f, 0.f, 2.f),
 		this->materials[0],
 		this->textures[TEX_PIC1],
-		this->textures[TEX_PIC2],
+		this->textures[TEX_PIC1_SPECULAR],
 		meshes
 	)
 	);
 
+	std::vector<Mesh*> cubeMeshes;
+	cubeMeshes.push_back(
+		new Mesh(
+			&Cube(),
+			glm::vec3(0.f, 0.f, 0.f),
+			glm::vec3(0.f),
+			glm::vec3(0.f),
+			glm::vec3(1.f)
+		)
+	);
 
+	this->models.push_back(
+		new Model(
+			glm::vec3(-1.f, 0.f, -10.f),
+			materials[0],
+			this->textures[TEX_PIC1],
+			this->textures[TEX_PIC1_SPECULAR],
+			cubeMeshes
+		)
+	);
 
+	this->models.push_back(
+		new Model(
+			glm::vec3(15.f, 15.f, -20.f),
+			materials[0],
+			this->textures[TEX_PIC1],
+			this->textures[TEX_PIC1_SPECULAR],
+			cubeMeshes
+		)
+	);
+
+	this->models.push_back(
+		new Model(
+			glm::vec3(-10.f, 0.f, -40.f),
+			materials[0],
+			this->textures[TEX_PIC1],
+			this->textures[TEX_PIC1_SPECULAR],
+			cubeMeshes
+		)
+	);
+
+	std::vector<Mesh*> groundMeshes;
+	groundMeshes.push_back(
+		new Mesh(
+			&Ground(this->camera.getMinBounds().y - 0.5f),
+			glm::vec3(0.f, 0.f, 0.f),
+			glm::vec3(0.f),
+			glm::vec3(0.f),
+			glm::vec3(1.f)
+		)
+	);
+
+	this->models.push_back(
+		new Model(
+			glm::vec3(-1.f, 0.f, 0.f),
+			materials[0],
+			this->textures[TEX_BG],
+			this->textures[TEX_BG_SPECULAR],
+			groundMeshes
+		)
+	);
+
+	//CLEANUP
 	for (auto& i : meshes)
 		delete i;
+	for (auto& i : cubeMeshes)
+		delete i;
+	for (auto& i : groundMeshes)
+		delete i;
+
 }
 
 void Game::initLights()
@@ -203,7 +273,13 @@ Game::Game(
 	const char* title,
 	const int WINDOW_WIDTH, const int WINDOW_HEIGHT,
 	const int GL_VERSION_MAJOR, const int GL_VERSION_MINOR,
-	bool resizable
+	bool resizable,
+	const float Xmin,
+	const float Xmax,
+	const float Ymin,
+	const float Ymax,
+	const float Zmin,
+	const float Zmax
 )
 	: WINDOW_WIDTH(WINDOW_WIDTH),
 	WINDOW_HEIGHT(WINDOW_HEIGHT),
@@ -211,7 +287,6 @@ Game::Game(
 	GL_VERSION_MINOR(GL_VERSION_MINOR),
 	camera(glm::vec3(0.f, 0.f, 1.f), glm::vec3(0.f, 0.f, 1.f), glm::vec3(0.f, 1.f, 0.f))
 {
-	//Camera cam();
 
 	//init variables
 	this->window = nullptr;
@@ -229,6 +304,8 @@ Game::Game(
 	this->mouseOffsetX = 0.0;
 	this->mouseOffsetY = 0.0;
 	this->firstMouse = true;
+
+	this->camera.set_movement_bounds(Xmin, Xmax, Ymin, Ymax, Zmin, Zmax);
 
 	this->initGLFW();
 	this->initWindow(title, resizable);
@@ -297,6 +374,7 @@ void Game::update()
 	this->models[0]->rotate(glm::vec3(0.f, 0.001f, 0.f));
 	this->models[1]->rotate(glm::vec3(0.f, 0.001f, 0.f));
 	this->models[2]->rotate(glm::vec3(0.f, 0.001f, 0.f));
+	this->models[3]->rotate(glm::vec3(0.f, 0.001f, 0.f));
 
 	/*if (this->mouseOffsetX != 0 || this->mouseOffsetY != 0)
 		std::cout << "DT : " << this->dt << "\n"
@@ -365,11 +443,20 @@ void Game::updateKeyboardInput()
 		//this->camPosition.x += 0.01f;
 		this->camera.move(this->dt, RIGHT);
 	}
-	if (glfwGetKey(this->window, GLFW_KEY_UP) == GLFW_PRESS) {
+
+	/*if (glfwGetKey(this->window, GLFW_KEY_UP) == GLFW_PRESS) {
 		this->camPosition.y += 0.01f;
 	}
 	if (glfwGetKey(this->window, GLFW_KEY_DOWN) == GLFW_PRESS) {
 		this->camPosition.y -= 0.01f;
+	}*/
+
+	//Speedup on space
+	if (glfwGetKey(this->window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		this->camera.setMovementSpeed(10.f);
+	}
+	else {
+		this->camera.setMovementSpeed(3.f);
 	}
 
 }
--- opengl_practice_1/Primitive.h
+++ opengl_practice_1/Primitive.h
@@ -49,9 +49,6 @@ public:
 			glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 0.f, 1.f),
 			glm::vec3(-0.5f, -0.5f, 0.f),		glm::vec3(0.f, 1.f, 0.f),	glm::vec2(0.f, 0.f),	glm::vec3(0.f, 0.f, 1.f),
 			glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),	glm::vec3(0.f, 0.f, 1.f),
-			glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(0.f, 1.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(0.f, 0.f, 1.f),
-
-
 		};
 		unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
@@ -125,5 +122,114 @@ public:
 		unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
 		this->set(vertices, nrOfVertices, nullptr, 0);
+	}
+};
+
+class Cube : public Primitive {
+public:
+	Cube(float len = 2)
+		:Primitive()
+	{
+		float coord = 0.5f * len;
+		Vertex vertices[] = {
+			//POSITION										//COLOR						//TEXCOORD				//NORMAL
+			//FRONT
+			glm::vec3(-coord, coord, coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(-coord, -coord, coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 0.f),	glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(coord, -coord, coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(coord, coord, coord),					glm::vec3(1.f, 0.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(0.f, 0.f, 1.f),
+			
+			
+			//LEFT
+			glm::vec3(-coord, coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 0.f),	glm::vec3(-1.f, 0.f, 0.f),
+			glm::vec3(-coord, -coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(-1.f, 0.f, 0.f),
+			glm::vec3(-coord, -coord, coord),				glm::vec3(1.f, 0.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(-1.f, 0.f, 0.f),
+			glm::vec3(-coord, coord, coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(-1.f, 0.f, 0.f),
+
+			//BACK
+			glm::vec3(-coord, coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 0.f, -1.f),
+			glm::vec3(-coord, -coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 0.f),	glm::vec3(0.f, 0.f, -1.f),
+			glm::vec3(coord, -coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 0.f, -1.f),
+			glm::vec3(coord, coord, -coord),				glm::vec3(1.f, 0.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(0.f, 0.f, -1.f),
+
+			//RIGHT
+			glm::vec3(coord, coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 0.f),	glm::vec3(1.f, 0.f, 0.f),
+			glm::vec3(coord, -coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(1.f, 0.f, 0.f),
+			glm::vec3(coord, -coord, coord),				glm::vec3(1.f, 0.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(1.f, 0.f, 0.f),
+			glm::vec3(coord, coord, coord),			        glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(1.f, 0.f, 0.f),
+
+			//TOP
+			glm::vec3(-coord, coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 1.f, 1.f),
+			glm::vec3(-coord, coord, coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 1.f, 0.f),
+			glm::vec3(coord, coord, coord),					glm::vec3(1.f, 0.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(0.f, 1.f, 0.f),
+			glm::vec3(coord, coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 1.f, 0.f),
+
+			//BOTTOM
+			glm::vec3(-coord, -coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, -1.f, 1.f),
+			glm::vec3(-coord, -coord, coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, -1.f, 0.f),
+			glm::vec3(coord, -coord, coord),				glm::vec3(1.f, 0.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(0.f, -1.f, 0.f),
+			glm::vec3(coord, -coord, -coord),			    glm::vec3(0.5f, 0.5f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, -1.f, 0.f),
+
+
+		};
+		unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
+
+		GLuint indices[] = {
+			//front
+			0,1,2,
+			0,2,3,
+
+			//left
+			4,5,6,
+			4,6,7,
+
+			//back
+			8,9,10,
+			8,10,11,
+
+			//right
+			12,13,14,
+			12,14,15,
+
+			//top
+			16, 17, 18,
+			16, 18, 19,
+
+			//bottom
+			20, 21, 22,
+			20, 22, 23,
+
+		};
+		unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
+
+		this->set(vertices, nrOfVertices, indices, nrOfIndices);
+	}
+};
+
+class Ground : public Primitive
+{
+private:
+public:
+	Ground(float height = 0.f, float x_extent = 1000.f, float z_extent=1000.f)
+		:Primitive()
+	{
+		Vertex vertices[] = {
+			//POSITION									//COLOR						//TEXCOORD				//NORMAL
+			glm::vec3(-x_extent, height, z_extent),		glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),	glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(-x_extent, height, -z_extent),	glm::vec3(0.f, 1.f, 0.f),	glm::vec2(0.f, 0.f),	glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(x_extent, height, -z_extent),		glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),	glm::vec3(0.f, 0.f, 1.f),
+			glm::vec3(x_extent, height, z_extent),		glm::vec3(0.f, 1.f, 0.f),	glm::vec2(1.f, 1.f),	glm::vec3(0.f, 0.f, 1.f),
+
+		};
+		unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
+
+		GLuint indices[] =
+		{
+			0, 1, 2,	//Triangle 1
+			0, 2, 3,	//Triangle 2
+		};
+		unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
+
+		this->set(vertices, nrOfVertices, indices, nrOfIndices);
 	}
 };
